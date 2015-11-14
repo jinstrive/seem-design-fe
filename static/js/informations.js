c23273ae5c4f03ff4ac0f99c3e6d66f6dba7c07d
jinxin <jinxin@qfpay.com>
2015-11-14 15:52:06 +0800

    注释

--- static/js/informations.js
+++ static/js/informations.js
@@ -3,26 +3,26 @@ var is_more = true;
 var total,pageSize,totalPage; //总记录数，每页显示数，总页数 
 
 function render_author_infos(data) {
-	var data = [
-		{
-			'nickname': '梁甜', 
-			'avator': '../static/image/avatar.png',
-			'english_name': 'MOSSIA', 
-			'descr': '<p>卡文克莱是一个全方位的设计师，不论是正牌、副牌或任何一个产品路线，他都有完整的企划想法。现在，他旗下一共有三个主要的服装路线，而配件则依服装表现来搭配设计。现年56岁的他在纽约时尚界的一言一行都被媒体注视着，有“纽约第七大道的王子”的封号。 </p>'
-		},				
-		{
-			'nickname': '梁甜', 
-			'avator': '../static/image/avatar.png',
-			'english_name': 'MOSSIA', 
-			'descr': '<p>卡文克莱是一个全方位的设计师，不论是正牌、副牌或任何一个产品路线，他都有完整的企划想法。现在，他旗下一共有三个主要的服装路线，而配件则依服装表现来搭配设计。现年56岁的他在纽约时尚界的一言一行都被媒体注视着，有“纽约第七大道的王子”的封号。 </p>'
-		},				
-		{
-			'nickname': '梁甜', 
-			'avator': '../static/image/avatar.png',
-			'english_name': 'MOSSIA', 
-			'descr': '<p>卡文克莱是一个全方位的设计师，不论是正牌、副牌或任何一个产品路线，他都有完整的企划想法。现在，他旗下一共有三个主要的服装路线，而配件则依服装表现来搭配设计。现年56岁的他在纽约时尚界的一言一行都被媒体注视着，有“纽约第七大道的王子”的封号。 </p>'
-		},		
-	]
+	// var data = [
+	// 	{
+	// 		'nickname': '梁甜', 
+	// 		'avator': '../static/image/avatar.png',
+	// 		'english_name': 'MOSSIA', 
+	// 		'descr': '<p>卡文克莱是一个全方位的设计师，不论是正牌、副牌或任何一个产品路线，他都有完整的企划想法。现在，他旗下一共有三个主要的服装路线，而配件则依服装表现来搭配设计。现年56岁的他在纽约时尚界的一言一行都被媒体注视着，有“纽约第七大道的王子”的封号。 </p>'
+	// 	},				
+	// 	{
+	// 		'nickname': '梁甜', 
+	// 		'avator': '../static/image/avatar.png',
+	// 		'english_name': 'MOSSIA', 
+	// 		'descr': '<p>卡文克莱是一个全方位的设计师，不论是正牌、副牌或任何一个产品路线，他都有完整的企划想法。现在，他旗下一共有三个主要的服装路线，而配件则依服装表现来搭配设计。现年56岁的他在纽约时尚界的一言一行都被媒体注视着，有“纽约第七大道的王子”的封号。 </p>'
+	// 	},				
+	// 	{
+	// 		'nickname': '梁甜', 
+	// 		'avator': '../static/image/avatar.png',
+	// 		'english_name': 'MOSSIA', 
+	// 		'descr': '<p>卡文克莱是一个全方位的设计师，不论是正牌、副牌或任何一个产品路线，他都有完整的企划想法。现在，他旗下一共有三个主要的服装路线，而配件则依服装表现来搭配设计。现年56岁的他在纽约时尚界的一言一行都被媒体注视着，有“纽约第七大道的王子”的封号。 </p>'
+	// 	},		
+	// ]
 	var authorsScript = $("#authors-list-template").html();
 	var authorsTemplate = Handlebars.compile(authorsScript);
 	$('#authors_list').append(authorsTemplate(data));
--- static/js/news.js
+++ static/js/news.js
@@ -3,23 +3,23 @@ var is_more = true;
 var total,pageSize,totalPage; //总记录数，每页显示数，总页数 
 
 function render_news(data) {
-	var data = [
-		{
-			'create_time': '2015-09-21', 
-			'title': '【未满】生活美学工作室', 
-			'content': '<p>一直想去的一家店 可能是期待值太高了吧 去过之后有些失望 套餐的东西还是挺多的 特别喜欢摆盘 除了主餐不满意 其他都还可以 应该不会再去了</p><p>#芝麻奶油面包# 面包松......</p><img src="../static/image/pic.png" alt="">'
-		},		
-		{
-			'create_time': '2015-09-21', 
-			'title': '【未满】生活美学工作室', 
-			'content': '<p>一直想去的一家店 可能是期待值太高了吧 去过之后有些失望 套餐的东西还是挺多的 特别喜欢摆盘 除了主餐不满意 其他都还可以 应该不会再去了</p><p>#芝麻奶油面包# 面包松......</p><img src="../static/image/pic.png" alt="">'
-		},
-		{
-			'create_time': '2015-09-21', 
-			'title': '【未满】生活美学工作室', 
-			'content': '<p>一直想去的一家店 可能是期待值太高了吧 去过之后有些失望 套餐的东西还是挺多的 特别喜欢摆盘 除了主餐不满意 其他都还可以 应该不会再去了</p><p>#芝麻奶油面包# 面包松......</p><img src="../static/image/pic.png" alt="">'
-		},		
-	]
+	// var data = [
+	// 	{
+	// 		'create_time': '2015-09-21', 
+	// 		'title': '【未满】生活美学工作室', 
+	// 		'content': '<p>一直想去的一家店 可能是期待值太高了吧 去过之后有些失望 套餐的东西还是挺多的 特别喜欢摆盘 除了主餐不满意 其他都还可以 应该不会再去了</p><p>#芝麻奶油面包# 面包松......</p><img src="../static/image/pic.png" alt="">'
+	// 	},		
+	// 	{
+	// 		'create_time': '2015-09-21', 
+	// 		'title': '【未满】生活美学工作室', 
+	// 		'content': '<p>一直想去的一家店 可能是期待值太高了吧 去过之后有些失望 套餐的东西还是挺多的 特别喜欢摆盘 除了主餐不满意 其他都还可以 应该不会再去了</p><p>#芝麻奶油面包# 面包松......</p><img src="../static/image/pic.png" alt="">'
+	// 	},
+	// 	{
+	// 		'create_time': '2015-09-21', 
+	// 		'title': '【未满】生活美学工作室', 
+	// 		'content': '<p>一直想去的一家店 可能是期待值太高了吧 去过之后有些失望 套餐的东西还是挺多的 特别喜欢摆盘 除了主餐不满意 其他都还可以 应该不会再去了</p><p>#芝麻奶油面包# 面包松......</p><img src="../static/image/pic.png" alt="">'
+	// 	},		
+	// ]
 	var newsListScript = $("#news-list-template").html();
 	var newsListTemplate = Handlebars.compile(newsListScript);
 	$('#news_list').append(newsListTemplate(data));
