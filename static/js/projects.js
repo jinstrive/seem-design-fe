66de626b4b72d04d584f22317d4fa8ac4569c61e
jinxin <jinxin@qfpay.com>
2015-12-05 14:44:19 +0800

    add scroll update

--- static/js/projects.js
+++ static/js/projects.js
@@ -40,7 +40,7 @@ function load_projects_data(){
 		$.ajax({ 
 			type: 'GET',
 			url: "/seems/all_pro_info", 
-			data: {'page_num': curPage + 1, 'page_size': 50, 'ptype': getUrlParam('ptype')}, 
+			data: {'page_num': curPage + 1, 'page_size': 9, 'ptype': getUrlParam('ptype')}, 
 			success: function(data_json){
 				projects_list = data_json.data;
 				console.log(projects_list);
@@ -69,5 +69,27 @@ function categorys_on_change(){
 	});
 }
 
-categorys_on_change();
-load_projects_data();
+
+$(document).ready(function () {  
+	categorys_on_change();
+	load_projects_data();
+
+    $(window).scroll(function () {  
+        var $currentWindow = $(window);  
+        //当前窗口的高度  
+        var windowHeight = $currentWindow.height();  
+        console.log("current widow height is " + windowHeight);  
+        //当前滚动条从上往下滚动的距离  
+        var scrollTop = $currentWindow.scrollTop();  
+        console.log("current scrollOffset is " + scrollTop);  
+        //当前文档的高度  
+        var docHeight = $(document).height();  
+        console.log("current docHeight is " + docHeight);  
+
+        //当 滚动条距底部的距离 + 滚动条滚动的距离 >= 文档的高度 - 窗口的高度  
+        //换句话说：（滚动条滚动的距离 + 窗口的高度 = 文档的高度）  这个是基本的公式  
+        if (scrollTop >= docHeight - windowHeight) {  
+            load_projects_data();  
+        }  
+    });  
+}); 
