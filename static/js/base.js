2b2ff11c66814e92f22a011c2e4a85845124d610
jinxin <jinxin@qfpay.com>
2015-12-07 22:15:49 +0800

    modis css

--- static/js/base.js
+++ static/js/base.js
@@ -3,13 +3,13 @@ function goTop()
     $(window).scroll(function(e) {
         //若滚动条离顶部大于100元素
         if($(window).scrollTop()>100)
-            $("#back-to-top").fadeIn(1000);//以1秒的间隔渐显id=gotop的元素
+            $("#back-to-top").show();//以1秒的间隔渐显id=gotop的元素
         else
-            $("#back-to-top").fadeOut(1000);//以1秒的间隔渐隐id=gotop的元素
+            $("#back-to-top").hide();//以1秒的间隔渐隐id=gotop的元素
     });
 };
+
 $(function(){
-	$("#back-to-top").hide()
     //点击回到顶部的元素
     $("#back-to-top").click(function(e) {
         //以1秒的间隔返回顶部
